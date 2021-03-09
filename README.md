0c9b469c48adf797d0473189efeb2fa6a324c839
pallavijakka1996 <jakkapallavi28@gmail.com>
2021-03-09 15:26:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-3aa7nt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-3aa7nt)
---
+++ src/app/products.ts
@@ -0,0 +1,20 @@
+export const products = [
+  {
+    id: 1,
+    name: "Real me",
+    price: 5809,
+    description: "Real me XT"
+  },
+  {
+    id: 2,
+    name: "Xiaomi",
+    price: 3489,
+    description: "Rmi note 5"
+  },
+  {
+    id: 3,
+    name: "Vivo",
+    price: 580,
+    description: "5 Oppo "
+  }
+];
